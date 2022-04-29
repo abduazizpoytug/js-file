25df82e9a0f713812f7dc2aaa16a5222f64cd210
abduaziz <104622399+abduazizpoytug@users.noreply.github.com>
2022-04-29 14:30:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let are = document.querySelector('#res')
+
+
+var name = 'abdullo';
+var lastName = 'olimov';
+let and = 'yoqubjon';
+
+are.innerHTML = `salom men ismim ${name} familyam ${lastName} otamning ismi ${and}`;
+
